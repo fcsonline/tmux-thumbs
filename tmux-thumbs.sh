656c2255ac58beb17dc8e59dfc330de624a3ba1a
Ferran Basora <fcsonline@gmail.com>
2019-03-05 23:41:35 +0000

    Use crate binary

--- tmux-thumbs.sh
+++ tmux-thumbs.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 CURRENT_PANE_ID=$(tmux list-panes -F "#{pane_id}:#{?pane_active,active,nope}" | grep active | cut -d: -f1)
-COMMAND="/home/ubuntu/dev/tmux-thumbs/target/debug/tmux-thumbs -a qwerty -r -u --tmux-pane ${CURRENT_PANE_ID}"
+COMMAND="tmux-thumbs -a qwerty -r -u --tmux-pane ${CURRENT_PANE_ID}"
 
 tmux new-window -d -n "[thumbs]" ${COMMAND}
 
