e498ef61bf1f3c78514a01ad1028b072e681d216
Ferran Basora <fcsonline@gmail.com>
2019-03-06 15:13:49 +0000

    Fix swaping

--- tmux-thumbs.sh
+++ tmux-thumbs.sh
@@ -2,9 +2,7 @@
 
 CURRENT_PANE_ID=$(tmux list-panes -F "#{pane_id}:#{?pane_active,active,nope}" | grep active | cut -d: -f1)
 COMMAND="tmux-thumbs -a qwerty -r -u --tmux-pane ${CURRENT_PANE_ID}"
+NEW_ID=$(tmux new-window -P -d -n "[thumbs]" ${COMMAND})
+NEW_PANE_ID=$(tmux list-panes -a | grep ${NEW_ID} | grep --color=never -o '%[0-9]\+')
 
-tmux new-window -d -n "[thumbs]" ${COMMAND}
-
-NEW_PANE_ID=$(tmux list-panes -a | grep 0:3.1 | grep -o '%[0-9]\+')
-
-tmux swap-pane -t ${NEW_PANE_ID}
+tmux swap-pane -d -s ${CURRENT_PANE_ID} -t ${NEW_PANE_ID}
