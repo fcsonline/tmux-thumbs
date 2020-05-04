53af67e6fac39f855b2112065d64d436edb0c745
Ferran Basora <fcsonline@gmail.com>
2020-05-04 17:15:00 +0000

    Fix path with ~

--- src/state.rs
+++ src/state.rs
@@ -9,7 +9,7 @@ const PATTERNS: [(&'static str, &'static str); 13] = [
   ("url", r"((https?://|git@|git://|ssh://|ftp://|file:///)[^ ]+)"),
   ("diff_a", r"--- a/([^ ]+)"),
   ("diff_b", r"\+\+\+ b/([^ ]+)"),
-  ("path", r"(([.\w\-@]+)?(/[.\w\-@]+)+)"),
+  ("path", r"(([.\w\-@~]+)?(/[.\w\-@]+)+)"),
   ("color", r"#[0-9a-fA-F]{6}"),
   ("uid", r"[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}"),
   ("ipfs", r"Qm[0-9a-zA-Z]{44}"),
@@ -222,6 +222,16 @@ mod tests {
     assert_eq!(results.get(0).unwrap().text.clone(), "/tmp/foo/bar_lol");
     assert_eq!(results.get(1).unwrap().text.clone(), "/var/log/boot-strap.log");
     assert_eq!(results.get(2).unwrap().text.clone(), "../log/kern.log");
+  }
+
+  #[test]
+  fn match_home() {
+    let lines = split("Lorem ~/.gnu/.config.txt, lorem");
+    let custom = [].to_vec();
+    let results = State::new(&lines, "abcd", &custom).matches(false, false);
+
+    assert_eq!(results.len(), 1);
+    assert_eq!(results.get(0).unwrap().text.clone(), "~/.gnu/.config.txt");
   }
 
   #[test]
