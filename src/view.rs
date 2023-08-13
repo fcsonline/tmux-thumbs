c0ed78beeea5909a51517275ea8c03962ad971ef
FliegendeWurst <2012gdwu+github@posteo.de>
2023-08-13 10:42:56 +0200

    Capture pane with ANSI escape codes
    Co-Authored-By: Ian Henry <ianthehenry@gmail.com>

--- src/view.rs
+++ src/view.rs
@@ -124,7 +124,7 @@ impl<'a> View<'a> {
       };
 
       // Find long utf sequences and extract it from mat.x
-      let line = &self.state.lines[mat.y as usize];
+      let line = &self.state.unescaped[mat.y as usize];
       let prefix = &line[0..mat.x as usize];
       let extra = prefix.width_cjk() - prefix.chars().count();
       let offset = (mat.x as u16) - (extra as u16);
